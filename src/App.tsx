8cdc544da0902a647267cadb519ac546a4015846
Aditya <adityakrsingh2604@gmail.com>
2024-06-10 23:38:04 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className="min-h-screen flex items-center justify-center bg-blue-100">
+        <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md space-y-4">
+          <h1 className="text-2xl font-bold text-center text-blue-600">Hello, Everyone</h1>
+          <p className="text-gray-500 text-center">
+            Basic Setup
+          </p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
