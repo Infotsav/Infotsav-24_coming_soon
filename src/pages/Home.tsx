9e1e76ea3afd544669c0f3ded6e10eef7e765451
Priyansh Rai <priyanshrai16@gmail.com>
2024-06-15 22:22:38 +0530

    Build initial intro section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,11 @@
-function Home() {   
-    return ( <>This is home page parrent of all</> );
+import Intro from "../components/Intro";
+
+function Home() {
+  return (
+    <main className="flex flex-col">
+      <Intro />
+    </main>
+  );
 }
 
 export default Home;
