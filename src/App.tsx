58d06a0ec26952e84c5e1d930ce599646b30fecc
Aditya <adityakrsingh2604@gmail.com>
2024-06-16 09:03:37 +0530

    feat: add hero section and sopnsor section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
 import gsap from "gsap";
 import ScrollTrigger from "gsap/ScrollTrigger";
 import { useLayoutEffect, useRef } from "react";
+
+
 import EventsCardsContainer from "./components/compounds/EventsCardsContainer";
+import Intro from "./components/Intro";
+import HeroSection from "./components/HeroSection";
+
 import "./App.css";
+import Sponsor from "./components/Sponsors";
+
 export default function App() {
   return (
     <div className="App">
+
+      <div className="header">
+        <HeroSection />
+      </div>
+
+      <main className="flex flex-col">
+        <Intro />
+      </main>
+
+      <div className="sponsor-section">
+        <Sponsor />
+      </div>
       <EventsCardsContainer />
     </div>
   );
---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,17 @@
+// import React from "react";
+import { WavyBackground } from "./ui/wavy-background";
+
+function HeroSection() {
+    return (
+        <WavyBackground className="mx-auto ">
+            <p className="text-vw-13 text-white font-black inter-var text-center">
+                Infotsav'24
+            </p>
+            {/* <p className="text-4xl mt-4 text-white font-normal inter-var text-center">
+                Coming soon ....
+            </p> */}
+        </WavyBackground>
+    );
+}
+
+export default HeroSection;
